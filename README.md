932ac85317f30ce9500d1c6a27d37433acbe4be4
Juan Antonio Brena Moral <bren@juanantonio.info>
2022-04-29 12:08:54 +0200

    Adding an initial working example

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# quarkus-amadeus-sdk-demo
+# Spring Boot + Amadeus Java SDK Demo
+
+## Motivation
+
+Sometimes, I need to travel in Europe or cross the Atlantic ocean but
+after 2 years of COVID, before travelling I need to review the country restrictions
+defined for travellers.
+
+## Value
+
+This example show how to see COVID restrictions in a particular [country](https://www.iso.org/iso-3166-country-codes.html).
+
+## How to use it?
+
+Register in [Amadeus for Developers](https://developers.amadeus.com) to get your `AMADEUS_CLIENT_ID` & `AMADEUS_CLIENT_SECRET`
+
+Export the values:
+
+```
+export AMADEUS_CLIENT_ID=YOUR_CLIENT_ID
+export AMADEUS_CLIENT_SECRET=YOUR_CLIENT_SECRET
+```
+
+## How to run in local?
+
+```
+./mvnw clean compile quarkus:dev
+./mvnw clean package
+java -jar target/quarkus-app/quarkus-run.jar
+curl 'http://localhost:8080/api/v1/covid-restrictions'
+curl 'http://localhost:8080/api/v1/covid-restrictions?country-code=FR'
+```
+
+# References
+
+- https://quarkus.io/guides/getting-started
---
+++ src/main/java/org/acme/CovidResource.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.exceptions.ResponseException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.Objects;
+
+@Path("/api/v1/covid-restrictions")
+public class CovidResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello(@QueryParam("country-code") String stateCode) throws ResponseException {
+
+        var AMADEUS_CLIENT_ID = System.getenv("AMADEUS_CLIENT_ID");
+        var AMADEUS_CLIENT_SECRET = System.getenv("AMADEUS_CLIENT_SECRET");
+
+        var amadeus = Amadeus.builder(AMADEUS_CLIENT_ID, AMADEUS_CLIENT_SECRET).setLogLevel("debug").build();
+        var countryCode = (!Objects.isNull(stateCode)) ? stateCode : "ES";
+
+        var params = Params.with("countryCode", countryCode);
+
+        return amadeus.dutyOfCare.diseases.covid19AreaReport.get(params).toString();
+    }
+}
